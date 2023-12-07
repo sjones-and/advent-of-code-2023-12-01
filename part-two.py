156644c9c4e0721f1896727b2cd7afbbb20824ae
Stuart Jones <47328516+sjones-and@users.noreply.github.com>
2023-12-07 22:23:45 +0000

    Add timing code

--- part-two.py
+++ part-two.py
@@ -2,6 +2,7 @@
 
 import os
 import string
+from time import perf_counter_ns
 
 numbers = {
     'one': '1',
@@ -23,6 +24,7 @@ def convert_numbers(input):
     return input
 
 def answer(input_file):
+    start = perf_counter_ns()
     delchars = str.maketrans('', '', ''.join([chr for chr in string.printable if not chr.isnumeric()]))
 
     with open(input_file, "r") as input:
@@ -30,8 +32,10 @@ def answer(input_file):
 
     cleaned = [convert_numbers(item).translate(delchars) for item in data]
     answer = sum([int(f'{item[0]}{item[-1]}') for item in cleaned])
+    end = perf_counter_ns()
 
     print(f'The answer is: {answer}')
+    print(f'{((end-start)/1000000):.2f} milliseconds')
 
 input_file = os.path.join(os.path.dirname(__file__), "input")
 answer(input_file)
